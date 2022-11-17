e867d80f9efb859500d6fef1cb06611cff55c91f
Aécio Guterres <aecioguterres@gmail.com>
2022-11-17 20:26:50 -0300

    Primeiro

---
+++ src/main/java/aetios/digital/peopleApiProject/PeopleApiProjectApplication.java
@@ -0,0 +1,13 @@
+package aetios.digital.peopleApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aetios/digital/peopleApiProject/PeopleApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package aetios.digital.peopleApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
